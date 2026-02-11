52a39396eb8f9cee0acbdd34c07208a88f6da558
zefparis <ben.barere@gmail.com>
2026-02-11 23:33:00 +0100

    f

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-import Script from 'next/script'
 import { ThemeProvider } from '@/components/theme-provider'
 import { Toaster } from '@/components/ui/toaster'
 import { Toaster as SonnerToaster } from 'sonner'
@@ -9,16 +8,20 @@ import { AuthSessionProvider } from '@/components/session-provider'
 import { ReactQueryProvider } from '@/components/query-provider'
 import { CookieBanner } from '@/components/cookie-banner'
 import { AIConsentBanner } from '@/components/ai-consent-banner'
+import { HcsWidget } from '@/components/HcsWidget'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: {
     default: 'PERSPECTA-COMPETENCES - Évaluation Professionnelle & Évaluation Cognitive',
-    template: '%s | PERSPECTA-COMPETENCES'
+    template: '%s | PERSPECTA-COMPETENCES',
   },
-  description: 'Plateforme premium d\'évaluation cognitive et professionnelle.',
-  metadataBase: new URL(process.env.NEXTAUTH_URL || 'https://perspecta.fr'),
+  description:
+    "Plateforme premium d'évaluation cognitive et professionnelle.",
+  metadataBase: new URL(
+    process.env.NEXTAUTH_URL ?? 'https://perspecta.fr'
+  ),
 }
 
 export default function RootLayout({
@@ -46,13 +49,7 @@ export default function RootLayout({
           <CookieBanner />
           <AIConsentBanner />
 
-          {/* HCS-U7 Widget v3 */}
-          <Script
-            src="https://widget.hcs-u7.online/v3/hcs-widget.js"
-            data-widget="HYtUigkEXGYU1cu8gzgTadzm2HjaOF7k"
-            strategy="afterInteractive"
-          />
-
+          <HcsWidget />
         </ThemeProvider>
       </body>
     </html>
