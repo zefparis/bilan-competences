178e14fcc091a4570326a87a994af0a8dc5d5e70
zefparis <ben.barere@gmail.com>
2026-02-12 09:21:12 +0100

    fix: use next/script beforeInteractive for reliable widget loading

--- src/components/HcsWidget.tsx
+++ src/components/HcsWidget.tsx
@@ -1,34 +1,14 @@
+'use client'
+
+import Script from 'next/script'
+
 export function HcsWidget() {
   return (
-    <>
-      <script
-        dangerouslySetInnerHTML={{
-          __html: `
-(function(){
-  window.__HCS_DIAG = [];
-  var origError = window.onerror;
-  window.onerror = function(msg, src, line, col, err) {
-    window.__HCS_DIAG.push({type:'error', msg:msg, src:src, line:line});
-    if (origError) return origError.apply(this, arguments);
-  };
-  var origFetch = window.fetch;
-  window.fetch = function() {
-    var url = arguments[0];
-    if (typeof url === 'string' && url.indexOf('hcs') !== -1) {
-      window.__HCS_DIAG.push({type:'fetch', url:url, time:Date.now()});
-    }
-    return origFetch.apply(this, arguments);
-  };
-})();
-`
-        }}
-      />
-      <script
-        src="https://hcs-widget-mvp.vercel.app/widget/v3/hcs-widget.js"
-        async
-        data-widget="qPtZJHNXf9CuP2LCLHNX8nkOlaFWq8tC"
-        data-debug="true"
-      />
-    </>
+    <Script
+      id="hcs-widget"
+      src="https://hcs-widget-mvp.vercel.app/widget/v3/hcs-widget.js"
+      strategy="beforeInteractive"
+      data-widget="qPtZJHNXf9CuP2LCLHNX8nkOlaFWq8tC"
+    />
   )
 }
