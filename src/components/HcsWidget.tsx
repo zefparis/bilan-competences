0bd4125d830d1b957fcb204f7b12a76893d41f9b
zefparis <ben.barere@gmail.com>
2026-02-12 09:18:50 +0100

    diag: add HCS diagnostic interceptor to capture widget errors and fetch calls

--- src/components/HcsWidget.tsx
+++ src/components/HcsWidget.tsx
@@ -1,9 +1,34 @@
 export function HcsWidget() {
   return (
-    <script
-      src="https://hcs-widget-mvp.vercel.app/widget/v3/hcs-widget.js"
-      async
-      data-widget="qPtZJHNXf9CuP2LCLHNX8nkOlaFWq8tC"
-    />
+    <>
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `
+(function(){
+  window.__HCS_DIAG = [];
+  var origError = window.onerror;
+  window.onerror = function(msg, src, line, col, err) {
+    window.__HCS_DIAG.push({type:'error', msg:msg, src:src, line:line});
+    if (origError) return origError.apply(this, arguments);
+  };
+  var origFetch = window.fetch;
+  window.fetch = function() {
+    var url = arguments[0];
+    if (typeof url === 'string' && url.indexOf('hcs') !== -1) {
+      window.__HCS_DIAG.push({type:'fetch', url:url, time:Date.now()});
+    }
+    return origFetch.apply(this, arguments);
+  };
+})();
+`
+        }}
+      />
+      <script
+        src="https://hcs-widget-mvp.vercel.app/widget/v3/hcs-widget.js"
+        async
+        data-widget="qPtZJHNXf9CuP2LCLHNX8nkOlaFWq8tC"
+        data-debug="true"
+      />
+    </>
   )
 }
